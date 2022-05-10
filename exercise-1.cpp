21e3a85f180a78b08566ec2c14daf519277f4c0f
Dilyan Grigorov <dido.grigorov@gmail.com>
2022-05-10 18:15:12 +0300

    Update exercise-1.cpp

--- exercise-1.cpp
+++ exercise-1.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 
-int main() {
-    // Write C++ code here
-    int a;
-    std::cin >> a;
-    int b;
-    std::cin >> b;
-    
-    if (a == b) {
-        std::cout << (a + b) * 3 << std::endl;
-    }
-    
-    else {
-        std::cout << a + b << std::endl;
-    }
+using namespace std;
 
-    return 0;
+int test(int x, int y)
+        {
+            return x == y ? (x + y)*3 : x + y;
+        }
+        
+        
+int main() 
+ {
+  cout << test(1, 2) << endl;  
+  cout << test(3, 2) << endl;  
+  cout << test(2, 2) << endl;  
+  return 0;    
 }
